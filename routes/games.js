67f831e7c3b0e0159d46d691a7af3a1103b7f26c
roanscriven-zamaqo <roan.scrivan@zamaqo.com>
2026-02-27 09:24:43 +0200

    added post for games

--- routes/games.js
+++ routes/games.js
@@ -18,4 +18,37 @@ router.get('/', function(req, res, next){
   }
 });
 
+router.post('/', [addNewGame, returnGameById]);  
+
+function addNewGame(req, res, next){
+  connection.query(
+    `INSERT INTO games
+      (title, platform, year)
+      VALUES (?, ?, ?)`, 
+      [req.body.title, req.body.platform, req.body.year],
+    queryResults
+  );
+
+  function queryResults(err, results, fields){
+    if (err) return next(err); 
+    req.body.id = results.insertId
+    return next(); 
+  }
+}
+
+function returnGameById(req, res, done){
+  connection.query(`
+    SELECT * FROM games
+    WHERE id = ?
+    `,
+    [req.body.id],
+    queryResults
+  ); 
+
+  function queryResults(err, results, fields){
+    if (err) return next(err); 
+    return res.json(results); 
+  }
+}
+
 module.exports = router;
